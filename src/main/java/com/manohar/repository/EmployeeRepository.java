3bd8ac02c6c67db5dc82a1a826cb964dc9f77043
manoharcse569 <manoharcse569@gmail.com>
2025-06-29 20:15:54 +0530

    added exception handling test cases for parsing employees from csv

--- src/main/java/com/manohar/repository/EmployeeRepository.java
+++ src/main/java/com/manohar/repository/EmployeeRepository.java
@@ -14,17 +14,17 @@ public class EmployeeRepository {
 	private static List<Employee> employees;
 
 	static {
-		employees = loadEmployees();
+		employees = loadEmployees("/employees.csv");
 	}
 	
 	public List<Employee> getEmployees() {
 		return employees;
 	}
 
-	private static List<Employee> loadEmployees() {
+	private static List<Employee> loadEmployees(String filePath) {
 		List<Employee> employees = new ArrayList<>();
 
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(EmployeeService.class.getResourceAsStream("/employees.csv")))) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(EmployeeService.class.getResourceAsStream(filePath)))) {
 			String line;
 			br.readLine();
 			while((line = br.readLine())!=null) {
@@ -34,7 +34,11 @@ public class EmployeeRepository {
 				}
 				employees.add(new Employee(Integer.parseInt(values[0]), values[1], values[2], Double.parseDouble(values[3]), values[4]==""? null:Integer.parseInt(values[4])));
 			}
-		} catch (Exception e) {
+		} 
+		catch (CSVParsingException e) {
+			throw e;
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 			throw new CSVParsingException("exception occurred while parsing csv file", e);
 		}
--- src/test/java/com/manohar/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/manohar/repository/EmployeeRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.manohar.repository;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.manohar.exception.CSVParsingException;
 import com.manohar.vo.Employee;
 
 public class EmployeeRepositoryTest {
@@ -14,7 +17,7 @@ public class EmployeeRepositoryTest {
 
 
 	@Test
-	public void test() {
+	public void getEmployeesTest() {
 		List<Employee> expectedEmployees = List.of(
 				new Employee(123, "Joe", "Doe", 60000d, null),
 				new Employee(124, "Martin", "Chekov", 45000d, 123),
@@ -23,9 +26,34 @@ public class EmployeeRepositoryTest {
 				new Employee(305, "Brett", "Hardleaf", 34000d, 300));
 
 		List<Employee> actualEmployees =	employeeRepository.getEmployees();
-		System.out.println(actualEmployees);
-
+		System.out.println("actualEmployees:"+actualEmployees);
 		Assert.assertEquals(expectedEmployees, actualEmployees);
+	}
+
+	@Test
+	public void loadEmployeesWithInvalidFormatExceptionTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
+		Method method = employeeRepository.getClass().getDeclaredMethod("loadEmployees", String.class);
+		method.setAccessible(true);
+
+		InvocationTargetException exception =	Assert.assertThrows(InvocationTargetException.class, ()-> {
+			method.invoke(employeeRepository, "/employees_invalid_format.csv");
+		});
+
+		CSVParsingException csvParsingException = (CSVParsingException) exception.getCause();
+		Assert.assertEquals("invalid csv file format. Please validate", csvParsingException.getMessage());
+	}
+	
+	@Test
+	public void loadEmployeesWithNumberFormatExceptionTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
+		Method method = employeeRepository.getClass().getDeclaredMethod("loadEmployees", String.class);
+		method.setAccessible(true);
+
+		InvocationTargetException exception =	Assert.assertThrows(InvocationTargetException.class, ()-> {
+			method.invoke(employeeRepository, "/employees_invalid_number.csv");
+		});
+
+		CSVParsingException csvParsingException = (CSVParsingException) exception.getCause();
+		Assert.assertEquals("exception occurred while parsing csv file", csvParsingException.getMessage());
 	}
 
 }
